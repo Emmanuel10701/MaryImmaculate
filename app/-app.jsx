12262b55d9c0cbc7da76cf14d5d24e69c82cc6a3
Emmanuel Makau <emmanuelmakau90@gmail.com>
2026-02-09 12:37:13 +0300

    modernize gallery cards and optimize form responsiveness

--- app/-app.jsx
+++ app/-app.jsx
@@ -3,6 +3,7 @@
 import { usePathname } from "next/navigation";
 import Footer from "./components/Foooter/page"; 
 import ModernNavbar from "./components/Navbar/page";
+import { useEffect } from "react";
 
 export default function ClientLayoutWrapper({ children }) {
   const pathname = usePathname();
@@ -10,6 +11,15 @@ export default function ClientLayoutWrapper({ children }) {
   const isMainDashboard = pathname === "/MainDashboard";
   const isStudentPortal = pathname === "/pages/StudentPortal";
   const isAdminLogin = pathname === "/pages/adminLogin";
+
+  // Add or remove zoom class based on route
+  useEffect(() => {
+    if (isMainDashboard) {
+      document.documentElement.classList.add("no-zoom");
+    } else {
+      document.documentElement.classList.remove("no-zoom");
+    }
+  }, [isMainDashboard]);
 
   return (
     <>
